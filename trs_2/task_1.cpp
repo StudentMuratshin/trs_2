5ae75a915d78dd5ddbe54e71d071d311cb200841
Ильuам Муратшин <PETA4OK96@mail.ru>
2023-04-01 09:21:08 +0500

    второе

--- trs_2/task_1.cpp
+++ trs_2/task_1.cpp
@@ -17,45 +17,48 @@ int main()
 {
 	ofstream u_1("C:\\Users\\PETA4\\Desktop\\3\\6_sem\\trs\\labs\\trs_2\\out\\u1_out.csv");
 
-	const int m = 1000;
-	const int n = 54000;
-	double M = 0, diff;
+	vector <int> m = { 10, 100, 1000, 10000 };
+	vector <int> n = { 540, 5400, 54000, 54000 };
 
-	vector < vector <double> > u(n, vector <double>(m));
-
-	double tau = 1. / (n - 1);
-	double h = 1. / (m - 1);
-
-	for (int i = 0; i < m; i++)
+	for (int k = 0; k < m.size(); k++)
 	{
-		u[0][i] = (i * i * h * h);
-	}
+		double M = 0, diff;
+		vector < vector <double> > u(n[k], vector <double>(m[k]));
 
-	for (int j = 1; j < n; j++)
-	{
-		for (int i = 1; i < m - 1; i++)
+		double tau = 1. / (n[k]);
+		double h = 1. / (m[k]);
+
+		for (int i = 0; i < m[k]; i++)
 		{
-			u[j][i] = tau / (4 * pi * pi * h * h) * (u[j - 1][i - 1] - 2 * u[j - 1][i] + u[j - 1][i + 1])
-				+ tau * sin(2 * pi * i * h) - tau / (2 * pi * pi) + u[j - 1][i];
+			u[0][i] = (i * i * h * h);
 		}
-		u[j][0] = - 2 * pi * h * (1 - exp(-tau * j)) + u[j][1];
-		u[j][m - 1] = (h * (3 + 2 * pi * (1 - exp(-j * tau))) + u[j][m - 2]) / (1 + h);
-	}
 
-	for (int j = 0; j < n; j++)
-	{
-		for (int i = 0; i < m; i++)
+		for (int j = 1; j < n[k]; j++)
 		{
-			//u_1 << j * tau << " " << i * h << " " << u[j][i] << endl;
-			diff = abs(u[j][i] - sol(j * tau, i * h));
-			if (diff > M) {
-				M = diff;
-				cout << j * tau << " " << i * h << " " << M << endl;
+			for (int i = 1; i < m[k] - 1; i++)
+			{
+				u[j][i] = tau / (4 * pi * pi * h * h) * (u[j - 1][i - 1] - 2 * u[j - 1][i] + u[j - 1][i + 1])
+					+ tau * sin(2 * pi * i * h) - tau / (2 * pi * pi) + u[j - 1][i];
+			}
+			u[j][0] = - 2 * pi * h * (1 - exp(-tau * j)) + u[j][1];
+			u[j][m[k] - 1] = (h * (3 + 2 * pi * (1 - exp(-j * tau))) + u[j][m[k] - 2]) / (1 + h);
+		}
+		for (int j = 0; j < n[k]; j++)
+		{
+			for (int i = 0; i < m[k]; i++)
+			{
+				//u_1 << j * tau << " " << i * h << " " << u[j][i] << endl;
+				diff = abs(u[j][i] - sol(j * tau, i * h));
+				if (diff > M) {
+					M = diff;
+					//cout << j * tau << " " << i * h << " " << M << endl;
+				}
 			}
 		}
+		cout << "Step h(x): " << h << " Step T(t): " << tau << " Error: " << M << endl;
 	}
 
-	cout << M;
+
 	u_1.close();
 	return 0;
 }
--- trs_2/task_2.cpp
+++ trs_2/task_2.cpp
@@ -8,43 +8,106 @@ constexpr auto pi = 3.14159265358979323846;
 
 using namespace std;
 
+void tridiagonal_matrix(const int n, const double* A, const double* B, const double* C, const double* D, double* X) {
+	double* P = new double[n - 1];
+	double* Q = new double[n];
+
+	// forward
+	P[0] = C[0] / B[0];
+	Q[0] = D[0] / B[0];
+	for (int i = 1; i < n; ++i) {
+		if (i < n - 1) P[i] = C[i] / (B[i] - A[i] * P[i - 1]);
+		Q[i] = (D[i] - A[i] * Q[i - 1]) / (B[i] - A[i] * P[i - 1]);
+	}
+
+	//backward
+	X[n - 1] = Q[n - 1];
+	for (int i = n - 2; i >= 0; --i) {
+		X[i] = Q[i] - P[i] * X[i + 1];
+	}
+	delete[] P;
+	delete[] Q;
+}
+
+double sol(double t, double x)
+{
+	return (1 - exp(-t)) * sin(2 * pi * x) + x * x;
+}
+
 int main()
 {
 	ofstream u_1("C:\\Users\\PETA4\\Desktop\\3\\6_sem\\trs\\labs\\trs_2\\out\\u1_out.csv");
 
-	const int m = 120;
-	const int n = 730;
+	vector <int> m = { 1000 };
+	vector <int> n = { 1000 };
 
-	vector < vector <double> > u(n, vector <double>(m));
 
-	double tau = 1. / (n - 1);
-	double h = 1. / (m - 1);
-
-	for (int i = 0; i < m; i++)
+	for (int k = 0; k < m.size(); k++)
 	{
-		u[0][i] = (i * i * h * h);
-	}
+		double M = 0, diff;
+		vector < vector <double> > u(n[k], vector <double>(m[k]));
 
-	for (int j = 1; j < n; j++)
-	{
-		u[j][0] = 2 * pi * (1 - exp(-tau * j));
-		for (int i = 1; i < m - 1; i++)
+		double tau = 1. / (n[k] - 1);
+		double h = 1. / (m[k] - 1);
+
+		for (int i = 0; i < m[k]; i++)
 		{
-			u[j][i] = tau / (4 * pi * pi * h * h) * (u[j - 1][i - 1] - 2 * u[j - 1][i] + u[j - 1][i + 1])
-				+ tau * sin(2 * pi * i * h) - tau / (2 * pi * pi) + u[j - 1][i];
+			u[0][i] = (i * i * h * h);
 		}
-		u[j][m - 1] = (h + u[j][m - 2]) / (1 + h);
-	}
 
-	for (int j = 0; j < n; j++)
-	{
-		for (int i = 0; i < m; i++)
+		double* A = new double[m[k]];
+		double* B = new double[m[k]];
+		double* C = new double[m[k]];
+		double* F = new double[m[k]];
+		double* U_next = new double[m[k]];
+
+		for (int i = 1; i < m[k] - 1; i++)
 		{
-			u_1 << j * tau << " " << i * h << " " << u[j][i] << endl;
+			A[i] = -1. / (4 * pi * pi * h * h);
+			B[i] = 1. / tau + 1. / (2 * pi * pi * h * h);
+			C[i] = -1. / (4 * pi * pi * h * h);
 		}
+
+		A[0] = 0;
+		B[0] = -1. / h;
+		C[0] = 1. / h;
+		A[m[k] - 1] = -1. / h;
+		B[m[k] - 1] = 1. + 1. / h;
+		C[m[k] - 1] = 0;
+		
+		for (int j = 1; j < n[k]; j++)
+		{	
+			double t = j * tau;
+			F[0] = 2 * pi * (1 - exp(-t));
+			for (int i = 1; i < m[k] - 1; i++)
+			{
+				double x = i * h;
+				F[i] = u[j - 1][i] / tau + sin(x) - 1. / 2. / pi / pi;
+			}
+			F[m[k] - 1] = 3 + 2 * pi * (1 - exp(-t));
+
+			//vector<double> U_next;
+			tridiagonal_matrix(m[k], A, B, C, F, U_next);
+
+			for (int i = 0; i < m[k]; i++)
+			{
+				u[j][i] = U_next[i];
+			}
+		}
+
+		for (int j = 0; j < n[k]; j++)
+		{
+			for (int i = 0; i < m[k]; i++)
+			{
+				//u_1 << j * tau << " " << i * h << " " << u[j][i] << endl;
+				diff = abs(u[j][i] - sol(j * tau, i * h));
+				if (diff > M) {
+					M = diff;
+					//cout << j * tau << " " << i * h << " " << M << endl;
+				}
+			}
+		}
+		cout << "Step h(x): " << h << " Step T(t): " << tau << " Error: " << M << endl;
 	}
-
-
-	u_1.close();
 	return 0;
 }
